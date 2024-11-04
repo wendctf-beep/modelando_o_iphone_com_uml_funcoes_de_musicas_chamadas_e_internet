06c6b652c31ba7fd0b67f8eb085bc9e4bcc78f2d
wendctf-beep <wendc1.wc@gmail.com>
2024-11-04 15:42:16 -0300

    Add files via upload

---
+++ NavegadorInternet.java
@@ -0,0 +1,7 @@
+package src;
+
+public interface NavegadorInternet {
+    public void exibirPagina(String url);
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+}
---
+++ iPhone.java
@@ -0,0 +1,50 @@
+package src;
+
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+
+    @Override
+    public void ligar(String numero) {
+        //Implementação aqui
+    }
+
+    @Override
+    public void atender() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        //Implementação aqui
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void atualizarPagina() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void tocar() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void pausar() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        //Implementação aqui
+    }
+}
